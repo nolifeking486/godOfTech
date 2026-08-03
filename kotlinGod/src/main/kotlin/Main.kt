1a84c3a71eb3bd2e8047a082974bc564ac9fdf24
DEVANSH SHARMA <83576606+nolifeking486@users.noreply.github.com>
2026-08-03 22:59:44 +0530

    switching to linux removing intellij for now

--- kotlinGod/src/main/kotlin/Main.kt
+++ kotlinGod/src/main/kotlin/Main.kt
@@ -1,28 +1,21 @@
-import kotlin.math.abs
-
 class Solution {
-	fun isUgly(n: Int): Boolean {
-		var t = abs(n)
-
-		while (t % 5 == 0 && t != 0) {
-			t /= 5;
+	fun hammingWeight(n: Int): Int {
+		var a = 1L
+		var b : Long = n.toLong()
+		var ans = 0
+		while(a<=b){
+			if(a and b != 0L){
+				ans++
+			}
+			a = a shl 1
 		}
-		while (t % 3 == 0 && t != 0) {
-			t /= 3;
-		}
-		while (t % 2 == 0 && t != 0) {
-			t /= 2;
-		}
-		if (t == 1) {
-			return true
-		}
-		return false;
+		return ans
 
 	}
 }
+fun main(){
 
-fun main() {
-	var solution = Solution()
+	var sol = Solution()
 	var n = readln().toInt()
-	println(solution.isUgly(n))
+	println(sol.hammingWeight(n))
 }
